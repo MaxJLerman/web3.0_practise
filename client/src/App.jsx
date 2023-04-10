fb8650784724b3f28b9a5d43fc68513b83c1808e
Max Lerman <mjl82601@gmail.com>
2023-04-10 22:11:29 +0100

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3x1 font-bold underline">
+        Hello cunt
+      </h1>
+    </div>
+  );
+};
+
+export default App;
