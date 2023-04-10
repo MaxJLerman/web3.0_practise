252e14f560c078c416ce24895d34cb97389336bb
Max Lerman <mjl82601@gmail.com>
2023-04-10 22:21:11 +0100

    file and folder structure set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3x1 font-bold underline">
-        Hello cunt
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
