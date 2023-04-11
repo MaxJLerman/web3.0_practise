f62eb332db78356f6de39b9ee7367c9e65a72bd4
Max Lerman <mjl82601@gmail.com>
2023-04-11 16:12:20 +0100

    added mobile navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import logo from "../../images/logo.png";
-import {Menu, MenuOpen} from '@mui/icons-material';
+import {Menu, MenuOpen, Close} from '@mui/icons-material';
 
 const NavbarItem = ({ title, classProps }) => (
   <li className={`mx-4 cursor ${classProps}`}>{title}</li>
@@ -12,15 +12,13 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   
   const array = ["Market", "Exchange", "Tutorials", "Wallets"];
+
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
-        {array.map((item, index) => (
-          <NavbarItem key={item + index} title={item} />
-        ))}
         <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
           Login
         </li>
@@ -28,9 +26,19 @@ const Navbar = () => {
       <div className="flex relative">
         {
           toggleMenu
-          ? <MenuOpen style={{ fontSize: 50, color: "white" }} onClick={() => setToggleMenu(false)} />
-          : <Menu style={{ fontSize: 50, color: "white" }} onClick={() => setToggleMenu(true)} />
+            ? <MenuOpen style={{ fontSize: 48 }} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
+            : <Menu style={{ fontSize: 48 }} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
         }
+        {toggleMenu && (
+          <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
+            <li className="text-xl w-full my-2">
+              <Close style={{ fontSize: 48 }} onClick={() => setToggleMenu(false)} />
+            </li>
+            {array.map((item, index) => (
+              <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   );
