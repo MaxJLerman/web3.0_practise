d685e63e0c9640adc695c8f9c91b79ae723c4388
Max Lerman <mjl82601@gmail.com>
2023-04-13 15:14:20 +0100

    stuck at error

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -8,20 +8,18 @@ export const TransactionContext = createContext();
 const { ethereum } = window; // from MetaMask
 
 const getEthereumContract = () => {
-  const provider = new ethers.providers.Web3Provider(ethereum);
+  const provider = new ethers.BrowserProvider(ethereum);
   const signer = provider.getSigner();
   const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-  console.log({
-    provider,
-    signer,
-    transactionContract
-  });
+  return transactionContract;
 };
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [formData, setFormData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
 
   const handleChange = (event, name) => {
     setFormData((previousState) => ({...previousState, [name]: event.target.value }));
@@ -67,7 +65,32 @@ export const TransactionProvider = ({ children }) => {
     try {
       if (!ethereum) return alert("Wallet is not connected, please connect to Metamask");
 
-      // get data from form
+      const { addressTo, amount, keyword, message } = formData;
+      const transactionContract = getEthereumContract();
+
+      const parsedAmount = ethers.parseEther(amount); // converts decimal to GWEI hexadecimal
+
+      await ethereum.request({
+        method: "eth_sendTransaction",
+        params: [{
+          from: currentAccount,
+          to: addressTo,
+          gas: "0x5208", // 21000 GWEI == 0.000021 ETH
+          value: parsedAmount._hex,
+        }]
+      });
+
+      const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+
+      setIsLoading(true);
+      console.log(`Loading transaction: ${transactionHash.hash}`);
+      await transactionHash.wait();
+
+      setIsLoading(false);
+      console.log(`Transaction success: ${transactionHash.hash}`);
+      
+      const transactionCount = await transactionContract.getTransactionCount();
+      setTransactionCount(transactionCount.toNumber());
     }
     
     catch (error) {
--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 contract Transactions {
   uint256 transactionCount; // holds the number of transactions
 
-  event Transfer(address sender, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+  event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
   struct TransferStruct {
     address sender;
